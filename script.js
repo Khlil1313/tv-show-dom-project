3d337263dd7476c9fd029e2a8e9c1f58c597302c
Khlil1313 <90422263+Khlil1313@users.noreply.github.com>
2023-05-08 15:14:09 +0100

    add the eposide list level 100

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 //You can edit ALL of the code here
+const allEpisodes = getAllEpisodes();
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
-}
 
+}
+ 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+ 
+  episodeList.map(el => {
+    let episodeDivContainer = document.createElement("div");
+    let episodeTitleElement = document.createElement("h3");
+    let episodeImageElement = document.createElement("img");
+    let episodeSummary = document.createElement("span");
+
+    episodeDivContainer.classList.add("divClass");
+    
+
+    episodeTitleElement.innerHTML = `${el.name}`;
+    episodeImageElement.src = `${el.image.medium}`;
+    episodeSummary.innerHTML = `${el.summary}`;
+
+    document.getElementById("root").appendChild(episodeDivContainer);
+    episodeDivContainer.appendChild(episodeTitleElement);
+    episodeDivContainer.appendChild(episodeImageElement);
+    episodeDivContainer.appendChild(episodeSummary);
+  })
+    
 }
 
 window.onload = setup;
+
+
